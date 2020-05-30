f155f91218dd46e1a0170929f31911d25db72d08
ekoulemaneng <ekoulemaneng@gmail.com>
2020-05-30 18:42:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send("/public/index.html"));
+
+
+app.get('/api/timestamp/', (req, res) => {
+    let date = new Date();
+    res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+});
+
+
+
+app.get('/api/timestamp/:date_string', (req, res) => {
+
+    let str = req.params.date_string;
+    let date;
+
+    if ((/^\d+$/).test(str)) {
+        date = new Date(parseInt(str));
+        res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+    }
+    else if ((/^\d{4}-\d{1,2}-\d{1,2}$/).test(str)) {
+        date = new Date(str);
+        if (date.toString() === "Invalid Date") res.json({"error" : "Invalid Date" });
+        else res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+    }
+    else res.json({"error": "Invalid Date" });  
+
+});
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.use(function(req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+});
+
+app.listen(3000);
---
+++ public/script.js
@@ -0,0 +1,13 @@
+const handleTime = () => {
+    let val = document.getElementById("inputValue").value;
+    let url = "http://localhost:3000/api/timestamp/" + val;
+    document.getElementById("inputValue").value = "";
+    fetch(url).then(res => res.json()).then(value => fillTable(value));
+}
+
+const fillTable = obj => {
+    if (obj.hasOwnProperty('error')) document.getElementById("table").innerHTML = '<tr class="red"><td>Error</td><td>:</td><td>' + obj["error"] + '</td></tr>';
+    else document.getElementById("table").innerHTML = '<tr><td>UNIX</td><td>:</td><td class="bold">' + obj["unix"] + '</td></tr>' + '<tr><td>UTC</td><td>:</td><td class="bold">' + obj["utc"] + '</td></tr>';
+}
+
+document.getElementById("sender").addEventListener("click", handleTime);
